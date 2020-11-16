19fc2009ec5869ffaa6762c7ef341efcc6c1fd7f
Pascal Martin <pascal.fb.martin@gmail.com>
2020-11-15 22:29:44 -0800

    Use an external index service (EXPERIMENTAL)

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 A sprinkler controller in C with a web interface
 
 ## Overview
-This project is a small sprinkler controller with a web interface. This project depends on [echttp](https://github.com/pascal-fb-martin/echttp), [houseportal](https://github.com/pascal-fb-martin/houseportal) and [houserelays](https://github.com/pascal-fb-martin/houserelays).
+This project is a small sprinkler controller with a web interface. This project depends on the following projects:
+* [echttp](https://github.com/pascal-fb-martin/echttp),
+* [houseportal](https://github.com/pascal-fb-martin/houseportal),
+* [waterwise](https://github.com/pascal-fb-martin/waterwise) and
+* [houserelays](https://github.com/pascal-fb-martin/houserelays).
 
 The primary intend is to create a sprinkler controller that:
 * Adjust watering automatically, based on an online index (Southern California) or on a season-based index table (monthly or weekly).
@@ -16,25 +20,30 @@ These goals are very similar those of the [Sprinkler](https://github.com/pascal-
 * Bringing all the sprinkler wires to a single place is not always easy. Following a home remodeling, I ended with one half of the wires in a separate location. This is handled by having two independent controllers and two programs that alternate the pulse and pause periods in a carefully balanced way.
 * Maintaining node.js on Debian, and a Javascript application in the always changing world of Node.js and ECMA standards is more hassle than I want to entertain.
 
-The Sprinkler application also includes complex features that did not pan out well and were not included in this project:
-* Google calendar based watering programs creates more complexity than it is worth, both in coding and configuration.
-* Weather based index calculation was never completed because it is difficult to find all the data required (especially sun radiation), and
+The original Sprinkler application also includes complex features that did not pan out well and are not supported in this project:
+* Google calendar based watering programs: this creates more complexity than it is worth, both in coding and configuration.
+* Weather based index calculation: this was never completed because it is difficult to find all the data required (especially sun radiation).
 * Weather web sites all have their annoyances: NOAA is bulky and bureaucratic, and its data is surprisingly not that rich, while the WeatherUnderground stations keep changing, with uneven data quality.
 
-This project is a complete rewrite of Sprinkler in the spirit of micro-services: it does less, but relies more on other web services. This makes it a more decentralized and flexible setup.
+This project is a complete rewrite of Sprinkler in the spirit of micro-services: it does less, and relies more on other web services to implement the missing features. This makes it a more decentralized and flexible setup.
 
 Instead of installing drivers for different types of relay/triac interfaces, the design relies on a generic web API, and the specific interface itself is implemented as a web service. The [houserelays](https://github.com/pascal-fb-martin/houserelays) web service is the first implementation that follows this new design. A benefit is that a new interface can be created outside of the HouseSprinkler code base, which is the essence of the web service philosophy. This also makes it possible to support multiple interfaces simultaneously, both geographically distributed and possibly using different hardware interfaces.
 
-Future plan is to create another microservice for weather information and index calculation, with versions possibly based on NOAA, WeatherUnderground, a local weather station or some other data source.
+Instead of implementing multiple weather and watering index interfaces, it relies on a generic web API, and the specific weather/index interface itself is implemented as a web service. The [waterwise](https://github.com/pascal-fb-martin/waterwise) web service is a minimal implementation that relies on the bewaterwise.com web site maintained by the Metropolitan Water District of Southern California (and is therefore of interest only for those in the Los Angeles and San Diego areas).
+
+Future plan is to create other microservices for weather information and index calculation, starting with an interface to the state of California's CIMIS web site and data.
 
 ## Installation
 
-* Install the [echttp](https://github.com/pascal-fb-martin/echttp), [houseportal](https://github.com/pascal-fb-martin/houseportal) and [houserelays](https://github.com/pascal-fb-martin/houserelays) projects
+* Install [echttp](https://github.com/pascal-fb-martin/echttp)
+* Install [houseportal](https://github.com/pascal-fb-martin/houseportal)
+* Install [waterwise](https://github.com/pascal-fb-martin/waterwise)
+* Install [houserelays](https://github.com/pascal-fb-martin/houserelays)
 * Clone this repository.
 * make rebuild
 * sudo make install
-* Create /etc/house/sprinkler.json.
 
 ## Configuration
 
-The HouseSprinkler configuration is defined in a JSON file, by default /etc/house/sprinkler.json.
+The HouseSprinkler configuration is defined in a JSON file, by default /etc/house/sprinkler.json. However the proper way to configure is to access the Config page on the sprinkler's web UI.
+
--- housesprinkler_index.c
+++ housesprinkler_index.c
@@ -54,28 +54,16 @@
  *
  * CONFIGURATION
  *
- * This module is driven by the list of index providers provided in the
- * JSON configuration. Each provider is defined by:
- * - a provider name.
- * - an enabled flag (a way to keep the configuration's data without
- *   activating it).
- * - an URL to get the index data.
- * - An indication if the data is XML or JSON format.
- * - A path to the index value, in JavaScript syntax.
- * - the minimum and maximum adjustment bounds for the index.
- * - A list of times of day when to query the server.
+ * This module searches for the services providing "waterindex", and requests
+ * an index from each of them. It reports the index with the highest priority
+ * that it knows of.
  *
- * The module does not query a server more often than every six hours,
+ * The module does not query a service more often than every hour,
  * regardless of the schedule provided. A watering index does not change
- * every hour anyway.
+ * frequently anyway.
  *
- * The module query the enabled servers from that list, in the order provided,
- * until one has answered with a valid index value. For the purpose of that
- * poll logic, an index value becomes stale after six hours.
- *
- * The most reasonable use of multiple servers is to use one as the primary
- * source (listed first) and then schedule a second one at the same time, or
- * an hour later, as a backup.
+ * The most reasonable use of multiple services is to use one as the primary
+ * source (highest priority) and set the others as backups (lower priority).
  */
 
 #include <string.h>
@@ -83,9 +71,9 @@
 
 #include <echttp.h>
 #include <echttp_json.h>
-#include <echttp_xml.h>
 
 #include "houselog.h"
+#include "housediscover.h"
 
 #include "housesprinkler.h"
 #include "housesprinkler_index.h"
@@ -93,113 +81,24 @@
 
 #define DEBUG if (echttp_isdebug()) printf
 
-typedef struct {
-    int  enabled;
-    const char *name;
-    const char *url;
-    int  format;
-    const char *path;
-    int  adjust[2];
-    int  refresh[24];
-    time_t lastquery;
-} SprinklerIndexServer;
+#define ONEDAY       86400
+#define DEFAULTINDEX   100
 
-#define INDEX_FORMAT_UNKNOWN 0
-#define INDEX_FORMAT_JSON    1
-#define INDEX_FORMAT_XML     2
-
-static SprinklerIndexServer *Servers = 0;
-static int                   ServersCount = 0;
-
-static int    SprinklerIndex = 100;
-static time_t SprinklerIndexTimestamp = 0;
-static const char *SprinklerIndexOrigin = "";
+static int         SprinklerIndex = DEFAULTINDEX;
+static int         SprinklerIndexPriority = 0;
+static time_t      SprinklerIndexTimestamp = 0;
+static char       *SprinklerIndexOrigin = 0;
 
 #define INDEX_MAX_LISTENER  16
 static housesprinkler_index_listener *SprinklerIndexListener[INDEX_MAX_LISTENER];
 
 void housesprinkler_index_refresh (void) {
-
-    int i, j;
-    int count;
-    int content;
-    char path[128];
-    int list[256];
-
-    // Reload all servers.
-    //
-    if (Servers) free (Servers);
-    ServersCount = 0;
-    content = housesprinkler_config_array (0, ".wateringindex");
-    if (content > 0) {
-        ServersCount = housesprinkler_config_enumerate (content, list);
-    }
-
-    Servers = calloc (ServersCount, sizeof(SprinklerIndexServer));
-    DEBUG ("Loading %d index servers\n", ServersCount);
-
-    for (i = 0; i < ServersCount; ++i) {
-        int child = list[i];
-        SprinklerIndexServer *server = Servers + i;
-
-        server->enabled = housesprinkler_config_boolean (child, ".enable");
-        server->name = housesprinkler_config_string (child, ".provider");
-        server->url = housesprinkler_config_string (child, ".url");
-        const char *format = housesprinkler_config_string (child, ".format");
-        server->path = housesprinkler_config_string (child, ".path");
-
-        if (!server->name || !server->url || !server->path || !format) {
-            server->enabled = 0;
-            DEBUG ("\tIndex server at %d disabled (incomplete record)\n", i);
-            continue;
-        }
-
-        server->lastquery = 0;
-        server->format = INDEX_FORMAT_JSON; // Default.
-        if (format) {
-            if (strcmp (format, "XML") == 0) server->format = INDEX_FORMAT_XML;
-            if (strcmp (format, "JSON") == 0) server->format = INDEX_FORMAT_JSON;
-            else {
-                DEBUG ("\tIndex server at %d disabled (invalid format %s)\n",
-                       i, format);
-            }
-        }
-
-        server->adjust[0] = housesprinkler_config_integer (child, ".adjust.min");
-        server->adjust[1] = housesprinkler_config_integer (child, ".adjust.max");
-        // If no limits provided, use reasonable numbers.
-        if (server->adjust[0] <= 0) server->adjust[0] = 0;
-        if (server->adjust[1] <= 0) server->adjust[1] = 150;
-
-        int refresh = housesprinkler_config_array (child, ".refresh");
-        if (refresh < 0) {
-            server->refresh[0] = 0; // Midnight.
-            for (j = 1; j < 24; ++j) server->refresh[j] = -1;
-            continue;
-        }
-        int times[24];
-        int timescount = housesprinkler_config_enumerate (refresh, times);
-        if (timescount > 24) {
-            houselog_trace (HOUSE_FAILURE, server->name,
-                            "More than 24 refresh times per day?");
-            exit (1); // Array overflow has occurred.
-        }
-        for (j = 0; j < timescount; ++j) {
-            server->refresh[j] = housesprinkler_config_integer (times[j], "");
-            if (server->refresh[j] < 0 || server->refresh[j] >= 24) {
-                DEBUG ("\tIndex server %s invalid time %d\n",
-                       server->refresh[j]);
-                server->refresh[j] = -1; // Ignore invalid time.
-            }
-        }
-        for (; j < 24; ++j) server->refresh[j] = -1;
-
-        DEBUG ("\tIndex server %s (url=%s, format = %s, min=%d, max=%d)\n",
-               server->name, server->url, format, server->adjust[0], server->adjust[1]);
-    }
+    // No static configuration at this time: based on service discovery.
 }
 
 const char *housesprinkler_index_origin (void) {
+    if (! SprinklerIndexOrigin ||
+        SprinklerIndexTimestamp + ONEDAY < time(0)) return "default";
     return SprinklerIndexOrigin;
 }
 
@@ -208,13 +107,13 @@ time_t housesprinkler_index_timestamp (void) {
 }
 
 int housesprinkler_index_get (void) {
+    if (SprinklerIndexTimestamp + ONEDAY < time(0)) return DEFAULTINDEX;
     return SprinklerIndex;
 }
 
 void housesprinkler_index_register (housesprinkler_index_listener *listener) {
 
     int i;
-
     for (i = 0; i < INDEX_MAX_LISTENER; ++i) {
         if (SprinklerIndexListener[i] == 0) {
             SprinklerIndexListener[i] = listener;
@@ -226,87 +125,84 @@ void housesprinkler_index_register (housesprinkler_index_listener *listener) {
 static void housesprinkler_index_response
                (void *origin, int status, char *data, int length) {
 
-   SprinklerIndexServer *server = (SprinklerIndexServer *) origin;
+   const char *service = (const char *) origin;
    ParserToken tokens[100];
    int  count = 100;
    time_t now = time(0);
-   int rawindex;
-
-   if (SprinklerIndexTimestamp > now - 3600) {
-       DEBUG ("Ignoring response from %s, received recent index already\n",
-              server->url);
-       return;
-   }
 
    if (status == 302) {
        static char url[256];
        const char *redirect = echttp_attribute_get ("Location");
        if (!redirect) {
-           houselog_trace (HOUSE_FAILURE, server->name,
-                           "%s: invalid redirect", url);
+           houselog_trace (HOUSE_FAILURE, service, "%s: invalid redirect", url);
            return;
        }
        strncpy (url, redirect, sizeof(url));
        const char *error = echttp_client ("GET", url);
        if (error) {
-           houselog_trace (HOUSE_FAILURE, server->name, "%s: %s", url, error);
+           houselog_trace (HOUSE_FAILURE, service, "%s: %s", url, error);
            return;
        }
-       echttp_submit (0, 0, housesprinkler_index_response, (void *)server);
+       echttp_submit (0, 0, housesprinkler_index_response, (void *)service);
        DEBUG ("Redirected to %s\n", url);
        return;
    }
 
    if (status != 200) {
-       houselog_trace (HOUSE_FAILURE, server->name, "HTTP code %d", status);
+       houselog_trace (HOUSE_FAILURE, service, "HTTP code %d", status);
        return;
    }
 
-   // Analyze the answer and retrieve the control points matching our zones.
    const char *error;
-   switch (server->format) {
-       case INDEX_FORMAT_JSON:
-           error = echttp_json_parse (data, tokens, &count); break;
-       case INDEX_FORMAT_XML:
-           error = echttp_xml_parse (data, tokens, &count); break;
-       default: return;
-   }
+   error = echttp_json_parse (data, tokens, &count);
    if (error) {
-       houselog_trace (HOUSE_FAILURE, server->name,
-                       "%s: syntax error, %s", server->url, error);
+       houselog_trace (HOUSE_FAILURE, service, "syntax error, %s", error);
        return;
    }
    if (count <= 0) return;
 
-   int index = echttp_json_search (tokens, server->path);
-   if (index <= 0) return;
+   int received = echttp_json_search (tokens, ".waterindex.status.received");
+   int priority = echttp_json_search (tokens, ".waterindex.status.priority");
+   if (received <= 0 || priority <= 0) return;
 
-   if (tokens[index].type == PARSER_INTEGER) {
-       rawindex = tokens[index].value.integer;
-   } else if (tokens[index].type == PARSER_STRING) {
-       // This happens with XML..
-       rawindex = atoi(tokens[index].value.string);
-   } else {
-       return; // Invalid.
-   }
-   if (SprinklerIndex < server->adjust[0])
-       SprinklerIndex = server->adjust[0];
-   else if (SprinklerIndex > server->adjust[1])
-       SprinklerIndex = server->adjust[1];
-   else
-       SprinklerIndex = rawindex;
+   time_t timestamp = (time_t) tokens[received].value.integer;
+   int    ipriority = tokens[priority].value.integer;
 
-   SprinklerIndexTimestamp = now;
-   SprinklerIndexOrigin = server->name;
+   // Ignore any new index if it is of lower priority, or too old.
+   //
+   if (ipriority < SprinklerIndexPriority ||
+       timestamp < SprinklerIndexTimestamp - ONEDAY ) return;
 
-   DEBUG ("Received index %d from %s\n", SprinklerIndex, server->name);
-   if (SprinklerIndex == rawindex)
-       houselog_event (now, "INDEX", server->name, "RECEIVED",
-                       "%d%% from %s", rawindex, server->url);
-   else
-       houselog_event (now, "INDEX", server->name, "RECEIVED",
-                       "%d%% (adjusted to %d%%) from %s",
-                       rawindex, SprinklerIndex, server->url);
+   // Ignore any new index of the same priority if it is older.
+   //
+   if (ipriority == SprinklerIndexPriority &&
+       timestamp <= SprinklerIndexTimestamp) return;
+
+   // This index seems to be a better one than we currently have:
+   // retrieve and store the data.
+   //
+   int index = echttp_json_search (tokens, ".waterindex.status.index");
+   int name = echttp_json_search (tokens, ".waterindex.status.name");
+   int source = echttp_json_search (tokens, ".waterindex.status.origin");
+   if (index <= 0 || name <= 0 || source <= 0) return;
+
+   SprinklerIndex = (int)(tokens[index].value.integer);
+
+   SprinklerIndexTimestamp = timestamp;
+   SprinklerIndexPriority = ipriority;
+
+   if (SprinklerIndexOrigin) free (SprinklerIndexOrigin);
+   SprinklerIndexOrigin = strdup (tokens[source].value.string);
+
+   // Now that we do got a brand new index, it is time to let everyone
+   // know about it.
+   //
+   DEBUG ("Received index %d from %s (service %s)\n",
+          SprinklerIndex, SprinklerIndexOrigin, service);
+
+   houselog_event (now, "INDEX", tokens[name].value.string, "UPDATE",
+                   "%d%% FROM %s (PRIORITY %d)",
+                   SprinklerIndex, SprinklerIndexOrigin, ipriority);
 
    int i;
    for (i = 0; i < INDEX_MAX_LISTENER; ++i) {
@@ -316,46 +212,49 @@ static void housesprinkler_index_response
    }
 }
 
-static void housesprinkler_index_query (SprinklerIndexServer *server) {
+static void housesprinkler_index_query
+                (const char *service, void *context, const char *provider) {
 
-    if (server->url[0] == 0) return;
+    char url[156];
 
-    DEBUG ("Requesting index from %s at %s\n", server->name, server->url);
-    const char *error = echttp_client ("GET", server->url);
+    snprintf (url, sizeof(url), "%s/status", provider);
+
+    DEBUG ("Requesting index from %s\n", url);
+    const char *error = echttp_client ("GET", url);
     if (error) {
-        houselog_trace (HOUSE_FAILURE, server->name, "%s", error);
+        houselog_trace (HOUSE_FAILURE, url, "%s", error);
         return;
     }
 
-    echttp_submit (0, 0, housesprinkler_index_response, (void *)server);
+    echttp_submit (0, 0, housesprinkler_index_response, (void *)url);
 }
 
 void housesprinkler_index_periodic (time_t now) {
 
-    static time_t LastCall = 0;
+    static time_t LastInquiry = 0;
 
     int i, h;
     struct tm local = *localtime(&now);
 
-    if (LastCall > now - 60) return; // Limit to one attempt per minute.
-    LastCall = now;
-
-    // Do not query any more server if we obtained an answer recently.
-    if (SprinklerIndexTimestamp > now - (6 * 3600)) return;
-
-    // Query the first server we did not ask yet.
-    for (i = 0; i < ServersCount; ++i) {
-        if (Servers[i].enabled) {
-            if (Servers[i].lastquery >= now - (6 * 3600)) continue;
-            for (h = 0 ; h < 24; ++h) {
-                if (local.tm_hour == Servers[i].refresh[h]) {
-                    housesprinkler_index_query (Servers+i);
-                    Servers[i].lastquery = now;
-                    return;
-                }
-            }
-        }
+    if (!SprinklerIndexTimestamp) {
+        // We do not know any index yet: try to get an index fast.
+        if (now < LastInquiry + 60) return; // Limit to one attempt per minute.
+    } else {
+        // We just want an update: go slower.
+        if (now < LastInquiry + 3600) return;
     }
+    LastInquiry = now;
+
+    // Forget a stale index.
+    //
+    if (now > SprinklerIndexTimestamp + ONEDAY) {
+        SprinklerIndexTimestamp = 0;
+        if (SprinklerIndexOrigin) free (SprinklerIndexOrigin);
+        SprinklerIndexOrigin = 0;
+    }
+
+    housediscovered ("waterindex", 0, housesprinkler_index_query);
+    housediscover ("waterindex");
 }
 
 int housesprinkler_index_status (char *buffer, int size) {
